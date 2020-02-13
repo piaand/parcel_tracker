8e9a8099faf0ddd245567525ee4e3159657c61f6
piaand <pia.m.andersin@gmail.com>
2020-02-13 14:24:19 +0200

    added the first practice files

---
+++ source/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello;
+public class HelloWorld {
+    public static void main(String[] args){
+		System.out.println("Hello World!!! sry that was rude");
+		NewWorld aladin = new NewWorld();
+		System.out.println(aladin.nimi);
+    }
+}
---
+++ source/hello/NewWorld.java
@@ -0,0 +1,8 @@
+package hello;
+public class NewWorld {
+	String nimi;
+
+	public NewWorld() {
+		nimi = "A whole new World! Aladin save mee";
+	} 
+}
